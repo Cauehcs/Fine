262426a51db51d50e0547bbb9977f6d0eff11afa
cauehcs <hcs.caue@gmail.com>
2022-06-09 13:24:49 -0300

    add(Divider): adicionado e estilizado

---
+++ src/components/divider/style.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { COLORS } from "../../constants";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 0px;
+  margin: 16px 0;
+  border: 1px solid #e6e6f0;
+`;
