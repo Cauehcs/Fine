4dd14cff16a1b776b51a2e98880c84e6729f6090
cauehcs <hcs.caue@gmail.com>
2022-06-13 12:43:55 -0300

    build(App): teste host firebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const firebaseConfig = {
+  apiKey: "AIzaSyDh3xrB7w-JdtM5o4EpF4lsymY_yXvAjg8",
+  authDomain: "fine-e6dea.firebaseapp.com",
+  projectId: "fine-e6dea",
+  storageBucket: "fine-e6dea.appspot.com",
+  messagingSenderId: "770886926044",
+  appId: "1:770886926044:web:16e4c9ac572bf883dfbe19",
+  measurementId: "G-T5D8P9CK6S",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
